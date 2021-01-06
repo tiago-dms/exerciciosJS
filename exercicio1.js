61f224e2f72e91ecba5a95168888220b5b1ef4eb
tiago-dms <tiago.dms@gmail.com>
2021-01-06 01:24:22 -0300

    adicionado funcao RetornaSoma

---
+++ exercicio1.js
@@ -0,0 +1,10 @@
+const valor1 = 2;
+const valor2 = 2;
+
+function RetornaSoma(parametro1, parametro2){
+    return parametro1 + parametro2;
+}
+
+var resultado = RetornaSoma(valor1, valor2);
+
+console.log(resultado);
